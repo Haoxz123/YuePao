940045ec9fd3ac8b44edb30ef8e9b14708f27283
Haoxz123 <543425474@qq.com>
2020-08-11 10:16:37 +0800

    本地hello1

---
+++ src/main/java/cn/kgc/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.kgc;
+
+/**
+ * @Author: HXZ
+ * @Date: 2020/8/11 9:59
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello1");
+
+    }
+
+}
